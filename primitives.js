cd2b59229d8a6b929998059ad480a7535252bcc1
rohit nagappa <rnagappa35@gmail.com>
2020-10-22 00:40:18 +0530

    boolean data type

---
+++ primitives.js
@@ -0,0 +1,8 @@
+//Booleans
+var authorized;
+authorized = false;
+authorized = null;
+authorized = undefined;
+authorized = true;
+authorized = (function () { return true; })();
+authorized = (function () { return false; })();
---
+++ primitives.ts
@@ -0,0 +1,16 @@
+//Booleans
+
+
+let authorized: boolean ;
+
+authorized = false;
+
+authorized  = null;
+
+authorized = undefined;
+
+authorized = true;
+
+authorized = (() => true) ();
+
+authorized =(() => false) ();
