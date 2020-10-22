5bf02748a29fc0739266a8332dd06c9a36c7bec1
rohit nagappa <rnagappa35@gmail.com>
2020-10-22 09:49:14 +0530

    number primitive type

--- primitives.js
+++ primitives.js
@@ -6,3 +6,15 @@ authorized = undefined;
 authorized = true;
 authorized = (function () { return true; })();
 authorized = (function () { return false; })();
+{
+    var myNumber_1;
+    myNumber_1 = 10;
+    myNumber_1 = -10;
+    myNumber_1 = 10.10;
+    myNumber_1 = -10.10;
+    myNumber_1 = 42;
+    myNumber_1 = 0x123;
+    myNumber_1 = null;
+    myNumber_1 = undefined;
+    myNumber_1 = (function () { return 83; })();
+}
--- primitives.ts
+++ primitives.ts
@@ -14,3 +14,17 @@ authorized = true;
 authorized = (() => true) ();
 
 authorized =(() => false) ();
+
+{
+    let myNumber :number;
+    myNumber  = 10;
+    myNumber = -10;
+    myNumber = 10.10;
+    myNumber  = -10.10;
+    myNumber = 0b101010;
+    myNumber  = 0x123;
+    myNumber = null;
+    myNumber = undefined;
+    myNumber = (() => 0o123)();
+    
+}
